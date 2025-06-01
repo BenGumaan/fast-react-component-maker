bcbcd08d380684f07ae37622f24da7d6c0831582
Ben_Gumaan <ben.gumaan@gmail.com>
2025-06-01 23:29:25 +0200

    Update eslint.config.mjs and package.json metadata for the new configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,31 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
-
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"],
+        }
+      ],
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
+      semi: "warn",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    }
+  }
+];
